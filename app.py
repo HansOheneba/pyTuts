d73cc596a8f1ff0b564a7508c3c1a4dbec1b7ae6
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2024-10-11 11:20:48 +0000

    Basic calc

---
+++ app.py
@@ -0,0 +1,5 @@
+name = input("Enter patient name: ")
+age = input("Enter patient age: ")
+by = 2024 -  int(age)
+birthYear = str(by)
+print("Patient name: " + name +"\n Patient birthyear: " + birthYear)
